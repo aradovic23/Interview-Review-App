a278e6bcd54ed58c23839e45ee9d3700a06a72b4
BIT Student <radovic.aleksa@gmail.com>
2021-06-24 16:44:30 +0200

    change home layout and create wizard page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Header.scss";
 import { Link } from "react-router-dom";
+import { SiWebmoney } from "react-icons/si";
 
 const Header = () => {
   return (
     <div className="nav-home">
-      <Link to="/">
-        <h1>Moneymaker</h1>
-      </Link>
+      <div id="moneymaker">
+        <Link to="/">
+          <h1>Moneymaker</h1>
+        </Link>
+        <SiWebmoney className="icon-header" />
+      </div>
       <Link to="/login">
         <button id="log-in-button">Log in</button>
       </Link>
--- src/pages/Wizard/Wizard.jsx
+++ src/pages/Wizard/Wizard.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Wizard.scss";
+import Header from "../../components/Header/Header";
 
 const Wizard = () => {
-  return <div>WIZARD</div>;
+  return (
+    <>
+      <Header />
+      <section className="wizard-section">
+        <div className="wizard">
+          <div id="left">
+            <h3>Lorem ipsum</h3>
+            <h3>Lorem ipsum</h3>
+            <h3>Lorem ipsum</h3>
+          </div>
+          <div id="right"></div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Wizard;
